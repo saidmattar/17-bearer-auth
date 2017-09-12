db86b78d1c5f194b571becaf0ce8ca4c5f2d2a39
saidmattar <ssabdou@gmail.com>
2017-09-12 14:18:45 -0700

    edited jest setup.js and the mocks.js files

--- lab-said/index.js
+++ lab-said/index.js
@@ -1,3 +1,4 @@
-MONGODB_URI = 'mongodb://said:said@ds133104.mlab.com:33104/cf-gram'
-APP_SECRET='superseekret'
-PORT = 4000
+'use strict';
+
+require('dotenv').config();
+require('./lib/server').start();
